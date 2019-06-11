5ab13089f638c5990a81194ee6b27157cb591a0d
Oleg Kolpashchikov <oleg.kolpashchikov@kraftvaerk.net>
2019-06-11 19:53:34 +0300

    init

---
+++ 01/01.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	//hello("Billy")
+	a, b := swap("b", "a")
+	//fmt.Println(a, b)
+	//fmt.Println("This was compiled with Go on Windows")
+	//a := "asdf"
+}
+
+// func hello(name string) {
+// 	fmt.Println("hello, " + name + " !")
+// }
+
+// x & y are of the same type - declare once
+// returns tuple
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+// func readFile() {
+// 	f, err = os.Open("file.txt")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer f.Close()
+// 	r := bufio.NewReader(f)
+// 	for {
+// 		s, err := r.ReadString('\n')
+// 	}
+
+}
